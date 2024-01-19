32e7b94b802a325a9523eb1bcc46bf8f1ec24784
Santhosh Puvaneswaran <64911148+sandyhandle@users.noreply.github.com>
2024-01-19 08:26:56 +0530

    The project is completed and login is working properly (#4)

--- finathon-application 2/src/main/java/com/sandyhandle/webapp/finathon/application/config/ApplicationConfig.java
+++ finathon-application 2/src/main/java/com/sandyhandle/webapp/finathon/application/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.sandyhandle.webapp.finathon.application.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,11 +32,15 @@ public class ApplicationConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-
     }
 
     @Bean
-    private PasswordEncoder passwordEncoder() {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
